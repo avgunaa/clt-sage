729989634cb9154b147c4af8eee6a3719f0c9d05
Gunaa AV <avgunaa@gmail.com>
2014-08-13 15:20:20 -0700

    basic clt using sage

---
+++ clt.py
@@ -0,0 +1,28 @@
+from sage.all import *
+from clt_params import *
+
+print "CLT implementation using SAGE" 
+
+print ZZ(1213120321)
+
+class MMP():
+    def __init__(self, lam, k):
+        return 0;
+
+    def encrypt()
+        return 0;
+
+    def sample()
+        return 0;
+    
+    def zero_test()
+        return 0;
+
+
+
+if __name__=="__main__":
+
+        mmap = MMP()
+        
+
+
---
+++ clt_params.py
@@ -0,0 +1,14 @@
+kappa = 6
+hBits = 80
+theta = 15
+
+sessionkeybits = 160
+alpha = 80
+
+N = 540
+delta = 23
+eta = 1838
+etp = 460
+
+rho = 41
+
