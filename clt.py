a11813be0ae7d0d2bf2cc58745fee3c3a7e99a7d
Gunaa AV <avgunaa@gmail.com>
2014-08-14 00:53:28 -0700

    skeletal code

--- clt.py
+++ clt.py
@@ -3,26 +3,46 @@ from clt_params import *
 
 print "CLT implementation using SAGE" 
 
-print ZZ(1213120321)
+current_time = lambda:time.time()
 
 class MMP():
     def __init__(self, lam, k):
-        return 0;
+        print "generate p_i's and x0: "
+        
+        print "generate crtCoeff_i's: "
+
+        print "generate the g_i's: "
+
+        print "generate z and zinv: "
+
+        print "generate y: "
+
+        print "generate zero tester v: "
+        
+        return 0
 
     def encrypt()
-        return 0;
+        return 0
 
     def sample()
-        return 0;
+        return 0
     
     def zero_test()
-        return 0;
+        return 0
 
-
+    def is_zero()
+        return 0
 
 if __name__=="__main__":
 
         mmap = MMP()
-        
+
+        print "generate encodings"
+
+        print "multiply encodings"
+
+        print "zero test"
+
+        c = current_time()
 
 
