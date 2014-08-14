f05faae410974df4900f3ae8dfce78f60aadf052
Gunaa AV <avgunaa@gmail.com>
2014-08-14 01:18:30 -0700

    updates to clt code

--- clt.py
+++ clt.py
@@ -7,9 +7,25 @@ current_time = lambda:time.time()
 
 class MMP():
     def __init__(self, lam, k):
+
+        self.x0 = ZZ(1)
+        self.p = [0 for i in range(N)]
+        
         print "generate p_i's and x0: "
+       
+        for i in range(N):
+            p[i] = next_prime(ZZ.random_element(2^eta))
+            
+        x0 = prod(p[i] for i in range(N))
+
+        
         
         print "generate crtCoeff_i's: "
+
+        crtCoeff = [0 for i in range(N)]
+        for i in range(N):
+            crtCoeff[i] = x0/p[i]
+
 
         print "generate the g_i's: "
 
@@ -21,16 +37,13 @@ class MMP():
         
         return 0
 
-    def encrypt()
+    def encrypt(self)
         return 0
 
-    def sample()
+    def sample(self)
         return 0
     
-    def zero_test()
-        return 0
-
-    def is_zero()
+    def is_zero(self,c)
         return 0
 
 if __name__=="__main__":
@@ -38,10 +51,17 @@ if __name__=="__main__":
         mmap = MMP()
 
         print "generate encodings"
+        encodings = [mmap.sample() for i in range(k)]
+
+
+        result = 1
 
         print "multiply encodings"
+        for c in encodings
+            result *= c
 
         print "zero test"
+        mmap.is_zero(result)
 
         c = current_time()
 
